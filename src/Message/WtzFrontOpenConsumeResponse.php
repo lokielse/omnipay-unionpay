c328f5036f28bfbe1aef40cffc58393c87c81476
QS <wic@qq.com>
2017-10-31 04:37:18 +0800

    Add Front Open Consume

---
+++ src/Message/WtzFrontOpenConsumeResponse.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Omnipay\UnionPay\Message;
+
+/**
+ * Class WtzFrontOpenConsumeResponse
+ * @package Omnipay\UnionPay\Message
+ */
+class WtzFrontOpenConsumeResponse extends WtzFrontOpenResponse
+{
+
+}
